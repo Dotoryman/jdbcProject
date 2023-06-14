d51db04d9883e251a46577105a6208ae0d948fae
admin <admin@DESKTOP-63ML1K3>
2023-06-14 14:20:04 +0900

    20230614 1420

--- jdbcProject/src/com/yedam/user/UserDao.java
+++ jdbcProject/src/com/yedam/user/UserDao.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.yedam.Dao;
 
@@ -31,7 +33,30 @@ public class UserDao {
 		}
 	}
 
-	// 조회
+	// 1. 추가
+	public boolean add(UserVO user) {
+		sql = "insert into tbl_users (user_id, user_pw, user_name) " + "values(?, ?, ?)";
+		conn = Dao.getConnect();
+		try {
+			psmt = conn.prepareStatement(sql); // new UserDao(); 처럼 하나하나 호출안하고 한방에(prepare 객체)
+			psmt.setString(1, user.getUserId());
+			psmt.setString(2, user.getUserPw());
+			psmt.setString(3, user.getUserName());
+
+			int r = psmt.executeUpdate();
+			if (r > 0) { // 수정사항 반영
+				return true;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return false;
+	}
+
+	// 2. 조회(id검색으로 1건 상세 조회)
 	public UserVO search(String userId) {
 		sql = "select * from tbl_users where user_id = ?";
 		conn = Dao.getConnect();
@@ -59,15 +84,45 @@ public class UserDao {
 
 	}
 
-	// 추가
-	public boolean add(UserVO user) {
-		sql = "insert into tbl_users (user_id, user_pw, user_name) " + "values(?, ?, ?)";
+	// 3. 수정
+	public boolean modify(UserVO user) {
+		sql = "update tbl_users " + "set user_pw = nvl(?, user_pw)," + "user_name = nvl(?, user_name),"
+				+ "user_birth = nvl(?, user_birth)," + "user_phone = nvl(?, user_phone),"
+				+ "user_addr = nvl(?, user_addr) " + "where user_id = ?";
+
 		conn = Dao.getConnect();
 		try {
 			psmt = conn.prepareStatement(sql); // new UserDao(); 처럼 하나하나 호출안하고 한방에(prepare 객체)
-			psmt.setString(1, user.getUserId());
-			psmt.setString(2, user.getUserPw());
-			psmt.setString(3, user.getUserName());
+			psmt.setString(1, user.getUserPw());
+			psmt.setString(2, user.getUserName());
+			psmt.setString(3, user.getUserBirth());
+			psmt.setString(4, user.getUserPhone());
+			psmt.setString(5, user.getUserAddr());
+			psmt.setString(6, user.getUserId());
+
+			int r = psmt.executeUpdate(); // 쿼리의 실행!
+			if (r > 0) {
+				return true;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return false;
+	}
+
+	// 4. 삭제
+	public boolean remove(String id) {
+		sql = "delete from tbl_users" + " where user_id = ?";
+		conn = Dao.getConnect();
+		try {
+//			conn.setAutoCommit(false); //예를들어 a에서 b로 송금할 때, a와 b 모두 수정이 성공하고나서 커밋해야함 
+//			conn.commit();
+
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
 
 			int r = psmt.executeUpdate();
 			if (r > 0) {
@@ -80,6 +135,36 @@ public class UserDao {
 			close();
 		}
 		return false;
+	}
+
+	// 5. 목록
+	public List<UserVO> list() {
+		List<UserVO> list = new ArrayList<>();
+
+		sql = "select * from tbl_users";
+		conn = Dao.getConnect();
+
+		try {
+			psmt = conn.prepareStatement(sql); // new UserDao(); 처럼 하나하나 호출안하고 한방에(prepare 객체)
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				UserVO user = new UserVO();
+				user.setUserId(rs.getString("user_id"));
+				user.setUserId(rs.getString("user_id"));
+				user.setUserPw(rs.getString("user_pw"));
+				user.setUserName(rs.getString("user_name"));
+				user.setUserBirth(rs.getString("user_birth"));
+				user.setUserPhone(rs.getString("user_phone"));
+				user.setUserAddr(rs.getString("user_addr"));
+				list.add(user);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return list;
 	}
 
 }
--- jdbcProject/src/com/yedam/user/UserMain.java
+++ jdbcProject/src/com/yedam/user/UserMain.java
@@ -1,46 +1,98 @@
 package com.yedam.user;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class UserMain {
 	public static void main(String[] args) {
-		
+
 		UserDao dao = new UserDao();
 		Scanner scn = new Scanner(System.in);
 		int menu = 0;
-		
-		while(true) {
-			System.out.println("============================================");
-			System.out.println("1 . 추가   2 . 조회  3 . 수정  4 . 삭제  5 . 종료");
-			System.out.println("============================================");
+
+		while (true) {
+			System.out.println("=======================================================");
+			System.out.println("1 . 추가   2 . 조회  3 . 수정  4 . 삭제  5 . 목록  6 . 종료");
+			System.out.println("=======================================================");
 			System.out.println("선택 > ");
-			menu = scn.nextInt();scn.nextLine();
-			if(menu == 1) {
-				System.out.printf("ID 를 입력하세요");
+			menu = scn.nextInt();
+			scn.nextLine();
+			if (menu == 1) { // 1. 추가
+				System.out.printf("생성하려는 ID 를 입력하세요");
 				String id = scn.nextLine();
 				System.out.printf("PW 를 입력하세요");
 				String pw = scn.nextLine();
 				System.out.printf("이름 을 입력하세요");
 				String name = scn.nextLine();
-				
+
 				UserVO user = new UserVO();
 				user.setUserId(id);
 				user.setUserPw(pw);
 				user.setUserName(name);
-				
-				if(dao.add(user)) {
+
+				if (dao.add(user)) {
 					System.out.println("처리성공");
 				} else {
 					System.out.println("처리실패");
 				}
-				
-			} else if(menu == 2) {
-				
-			} else if(menu == 3) {
-				
-			} else if(menu == 4) {
-				
-			} else if(menu == 5) {
+
+			} else if (menu == 2) {// 2. ID기준 한개 상세조회
+				System.out.printf("조회하려는 ID 를 입력하세요");
+				String id = scn.nextLine();
+
+				UserVO user = dao.search(id);
+				if (user == null) {
+					System.out.println("조회하려는 ID는 존재하지 않습니다.");
+				} else {
+					System.out.println(user.toString());
+				}
+
+			} else if (menu == 3) {// 3. 수정
+				System.out.printf("정보를 수정하려는 ID 를 입력하세요");
+				String id = scn.nextLine();
+				System.out.printf("수정하려는 PW 를 입력하세요");
+				String pw = scn.nextLine();
+				System.out.printf("수정하려는 이름 를 입력하세요");
+				String name = scn.nextLine();
+				System.out.printf("수정하려는 생년월일 을 입력하세요");
+				String birth = scn.nextLine();
+				System.out.printf("수정하려는 전화번호 를 입력하세요");
+				String phone = scn.nextLine();
+				System.out.printf("수정하려는 주소 를 입력하세요");
+				String addr = scn.nextLine();
+
+				UserVO user = new UserVO();
+				user.setUserId(id);
+				user.setUserAddr(addr);
+				user.setUserPhone(phone);
+
+				if (dao.modify(user)) {
+					System.out.println("수정이 정상적으로 완료되었습니다");
+				} else {
+					System.out.println("수정하려는 ID는 존재하지 않습니다");
+				}
+			} else if (menu == 4) { // 4. 삭제
+				System.out.printf("정보를 삭제하려는 ID 를 입력하세요");
+				String id = scn.nextLine();
+
+				if (dao.remove(id)) {
+					System.out.println("삭제가 정상적으로 완료되었습니다");
+				} else {
+					System.out.println("삭제하려는 ID는 존재하지 않습니다");
+				}
+
+			} else if (menu == 5) { // 목록
+				List<UserVO> list = dao.list();
+
+				if (list.size() == 0) {
+					System.out.println("조회결과 없음");
+				} else {
+					for (UserVO user : list) {
+						System.out.println(user.toString());
+					}
+				}
+
+			} else if (menu == 6) {
 				break;
 			}
 		}
